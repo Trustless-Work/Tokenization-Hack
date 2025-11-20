59259ff35ed0c02d69a90dedd3683c33cc5ec82a
JoelVR17 <vermudo.com@gmail.com>
2025-11-20 14:32:45 -0600

    feat(backoffice): trustless-work blocks initialization

--- apps/backoffice-tokenization/src/app/layout.tsx
+++ apps/backoffice-tokenization/src/app/layout.tsx
@@ -1,6 +1,14 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { ReactQueryClientProvider } from "@/components/tw-blocks/providers/ReactQueryClientProvider";
+import { TrustlessWorkProvider } from "@/components/tw-blocks/providers/TrustlessWork";
+import { EscrowProvider } from "@/components/tw-blocks/providers/EscrowProvider";
+import { EscrowDialogsProvider } from "@/components/tw-blocks/providers/EscrowDialogsProvider";
+import { EscrowAmountProvider } from "@/components/tw-blocks/providers/EscrowAmountProvider";
+import { Toaster } from "@/components/ui/sonner";
+import { WalletProvider } from "@/components/tw-blocks/wallet-kit/WalletProvider";
+import type { ReactNode } from "react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -20,14 +28,32 @@ export const metadata: Metadata = {
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: ReactNode;
 }>) {
   return (
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ReactQueryClientProvider>
+          <TrustlessWorkProvider>
+            <WalletProvider>
+              <EscrowProvider>
+                <EscrowDialogsProvider>
+                  <EscrowAmountProvider>
+                    <div className="relative flex min-h-screen w-full">
+                      <div className="flex-1 flex flex-col w-full">
+                        <div className="container mx-auto">{children}</div>
+                      </div>
+                    </div>
+
+                    <Toaster position="top-right" richColors />
+                  </EscrowAmountProvider>
+                </EscrowDialogsProvider>
+              </EscrowProvider>
+            </WalletProvider>
+          </TrustlessWorkProvider>
+        </ReactQueryClientProvider>
       </body>
     </html>
   );
