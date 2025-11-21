6cad8e63c2e9b623f7da2872e88ffd7e8a4d5c53
JoelVR17 <vermudo.com@gmail.com>
2025-11-20 19:09:38 -0600

    feat(product): add home view

---
+++ apps/project-updates-tokenization/src/features/home/HomeView.tsx
@@ -0,0 +1,9 @@
+import { GlowingEffectDemo } from "./HeroSection";
+
+export const HomeView = () => {
+  return (
+    <div className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start mx-30">
+      <GlowingEffectDemo />
+    </div>
+  );
+};
